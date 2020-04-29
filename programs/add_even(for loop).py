6eba88a482ff99a299e9889e3fb5124b4d8de4f2
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-04-29 14:53:56 +0530

    Create add_even(for loop).py

---
+++ programs/add_even(for loop).py
@@ -0,0 +1,16 @@
+>>Write a python script to add only even numbers.
+n=int(input("enter the number:"))
+i=0
+for i in range(x):
+     if i%2==0:
+        print(i)
+        i=i+2
+        
+    #output:
+    enter the number:10
+    0
+    2
+    4
+    6
+    8
+    
