9e45c5e36305892b7c8f24ed5c99a496eec69362
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-04-29 14:13:43 +0530

    Create for loop.py

---
+++ programs/for loop.py
@@ -0,0 +1,13 @@
+>>for Loops 
+ 
+#The for loop is commonly used to repeat some code a certain number of times. This is done by combining for loops with range objects.
+
+for i in range(5): 
+  print("hello!")
+
+Output:
+hello! 
+hello! 
+hello! 
+hello! 
+hello!
