bd82e03440a7d3096d4265e728b4daceb86fef60
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-04-30 15:56:52 +0530

    Create list_sum.py

---
+++ programs/list_sum.py
@@ -0,0 +1,12 @@
+# Program to find the sum of all numbers stored in a list
+
+
+sum=0
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11] 
+for val in numbers: 
+  sum = sum+val 
+print("The sum is", sum)
+
+
+#output:
+The sum is 48.
