33760b2e796e8e9dd1ac224aaac03a2c1306177c
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-05-01 18:45:31 +0530

    Create combined_p2.py

---
+++ combined_p2.py
@@ -0,0 +1,26 @@
+#execute this code change the parameters
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple','banana']
+
+print(fruits.count('apple'))
+
+print(fruits.count('tangerine'))
+
+print(fruits.index('banana'))
+
+print(fruits.reverse())
+
+print(fruits.append('grape'))
+
+print(fruits.sort())
+
+print(fruits.pop())
+
+
+#OUTPUT:
+2
+0
+3
+['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange']
+['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange', 'grape']
+['apple', 'apple', 'banana', 'banana', 'grape', 'kiwi', 'orange', 'pear']
+'pear'
