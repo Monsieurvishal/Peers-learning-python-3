d6a5e76ac1f425237d4b58f6712874733ef44e0b
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-05-01 18:22:36 +0530

    Create Range.py

---
+++ programs/Range.py
@@ -0,0 +1,21 @@
+>>Range 
+ 
+>>The range function creates a sequential list of numbers. 
+#The code below generates a list containing all of the integers, up to 10.
+
+#Program:
+
+numbers = list(range(10)) 
+print(numbers)
+
+#Output:
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Program:
+ numbers = list(range(3, 8)) 
+print(numbers) 
+ 
+#Output:
+
+[3, 4, 5, 6, 7]
