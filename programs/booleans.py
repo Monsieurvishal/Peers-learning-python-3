fcb41a53766709d26089caf62bcca55ee63076e4
Shreya Chandrashekar Shetty <60577378+ShreyaChandrashekarShetty@users.noreply.github.com>
2020-05-01 22:15:38 +0530

    Create booleans.py

---
+++ programs/booleans.py
@@ -0,0 +1,5 @@
+
+Booleans 
+ 
+Another type in Python is the Boolean type. There are two Boolean values: True and False. 
+They can be created by comparing values, for instance by using the equal operator ==.
