d45bb275d9a39b109f7de5ece2409b5cac7dd142
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-05-01 15:56:26 +0530

    Create list_p5.py

---
+++ programs/list_p5.py
@@ -0,0 +1,17 @@
+>>Write a python script to append 5 elements from user to a list using for loop .
+
+alist=[]
+for val in range(5):
+    x=int(input())
+    alist.append(x)
+print(alist)
+
+
+#output:
+2
+3
+5
+1
+1
+[2,3,5,1,1]
+      
