ccfd4b476b8c5ad896d4935304c5abf0fb8180ea
Shreya Chandrashekar Shetty <60577378+ShreyaChandrashekarShetty@users.noreply.github.com>
2020-05-01 22:27:50 +0530

    Create write python script for given question.py

---
+++ programs/write python script for given question.py
@@ -0,0 +1,12 @@
+
+Write a python script to enter the marks and print as given below.
+
+Take input from user 
+Less than 40 marks print fail
+
+
+40 to 50   print good
+50 to 60 marks print  very good 
+60 to 70 marks print excellent
+70 to 80 marks print fantastic
+Above 80 print marvelous
