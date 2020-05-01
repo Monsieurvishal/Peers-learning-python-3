a97a92661f2d4a4f970c79ac5dab6f5e86d55945
Shrustigowda <60534308+Shrustigowda@users.noreply.github.com>
2020-05-01 18:44:12 +0530

    Create combined_p1.py

---
+++ combined_p1.py
@@ -0,0 +1,34 @@
+#fruits is a list containing elements 
+
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple','banana']
+
+#count:This returns the repeated elements from the list.For example 
+apple is repeated 2 times so it reurns 2
+
+#Input:fruits.count('apple')
+#Output:2
+
+#Input:fruits.count('tangerine')
+#Output:0
+
+#index:This returns the index or position of the element in the list
+#Input:fruits.index('banana')
+#Output:3
+
+
+#reverse:reveres the element in the list
+#Input:fruits.reverse()
+#Output:['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange']
+
+#append:Adds the elemennt to the list
+#Input:fruits.append('grape')
+#Output:['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange', 'grape']
+
+#sort:Sorts the element according to alphabetical order
+#Input:fruits.sort()
+#Output:['apple', 'apple', 'banana', 'banana', 'grape', 'kiwi', 'orange', 'pear']
+ 
+#Pop means removing the last element
+#pop and append are opposite to each other
+#Input:fruits.pop()
+#Output:'pear'
