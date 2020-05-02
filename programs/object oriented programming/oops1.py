230710cd69cce52bed3327a87dd88d554426992e
Sherry1coder <64684306+Sherry1coder@users.noreply.github.com>
2020-05-02 15:28:05 +0530

    Create oops1.py

---
+++ programs/object oriented programming/oops1.py
@@ -0,0 +1,22 @@
+x = 42
+type(x)
+#<class 'int'>
+
+
+ y = 4.34
+ type(y)
+#<class 'float'>
+
+
+ def f(x):
+    return x + 1
+
+type(f)
+#<class 'function'>
+
+
+import math
+type(math)
+#<class 'module'>
+
+# this must give you an idea about the class.Python is an object oriented language everything here is class
